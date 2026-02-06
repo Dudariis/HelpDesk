4b2f13d5256ad4848e852bd22854652622b81d44
Dudariis <dudasilva51792@gmail.com>
2026-02-06 15:25:43 -0300

    atualizando

---
+++ Tecnico.cs
@@ -0,0 +1,35 @@
+using System;
+namespace HelpDesk;
+
+public class Tecnico : Usuario
+{
+    public string mudarStatus;
+    public string matricula;
+
+
+    public string MudarStatus
+    {
+        get { return mudarStatus; }
+        set { mudarStatus = value; }
+    }
+
+    public string matriculaTecnico
+    {
+        get { return matricula; }
+        set { matricula = value; }
+    }
+
+
+    public Tecnico(int id, string nome, string email, string mudarStatus, string matriculaTecnico) : base(id, nome, email)
+    {
+        this.mudarStatus = mudarStatus;
+        this.matricula = matriculaTecnico;
+    }
+
+    public new void ExibirDados()
+    {
+        base.ExibirDados();
+        Console.WriteLine($"Mudar Status: {mudarStatus}");
+        Console.WriteLine($"Matrícula: {matricula}");
+    }
+}
--- obj/Debug/net10.0/HelpDesk.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelpDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b3536eab3efd45be3da57cd89f867478faaf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
